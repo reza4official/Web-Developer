2a4b845c55b4dbc97ac653b695dce74deab356b1
Moch. Rheza Wira Z. Hasbi <reza4official@gmail.com>
2021-02-11 22:22:52 +0800

    Add roles admin/user login and register form

--- Pengembara Website/Backend/Laravel/Pengembara/resources/views/includes/navbar.blade.php
+++ Pengembara Website/Backend/Laravel/Pengembara/resources/views/includes/navbar.blade.php
@@ -50,17 +50,45 @@
         </li>
         </ul>
 
-        <!-- Mobile button -->
-        <form class="form-inline d-sm-block d-md-none">
-        <button class="btn btn-login my-2 my-sm-0 px-4">Masuk</button>
-        </form>
+        @guest
+            <!-- Mobile button -->
+            <form class="form-inline d-sm-block d-md-none">
+                @csrf
+                <button class="btn btn-login my-2 my-sm-0 px-4" type="button"
+                onclick="event.preventDefault(); location.href= '{{ url('login') }}';">
+                    Masuk
+                </button>
+            </form>
+    
+            <!-- Desktop button -->
+            <form class="form-inline my-2 my-lg-0 d-none d-md-block">
+                @csrf
+                <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="button"
+                onclick="event.preventDefault(); location.href= '{{ url('login') }}';">
+                    Masuk
+                </button>
+            </form>
+        @endguest
 
-        <!-- Desktop button -->
-        <form class="form-inline my-2 my-lg-0 d-none d-md-block">
-        <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">
-            Masuk
-        </button>
-        </form>
+        @auth
+            <!-- Mobile button -->
+            <form class="form-inline d-sm-block d-md-none" action="{{ url('logout') }}" method="POST">
+                @csrf
+                <button class="btn btn-login my-2 my-sm-0 px-4" type="submit">
+                    Keluar
+                </button>
+            </form>
+    
+            <!-- Desktop button -->
+            <form class="form-inline my-2 my-lg-0 d-none d-md-block" action="{{ url('logout') }}" method="POST">
+                @csrf
+                <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="submit">
+                    Keluar
+                </button>
+            </form>
+        @endauth
+
+        
     </div>
     </nav>
 </div>
--- Pengembara Website/Backend/Laravel/Pengembara/routes/web.php
+++ Pengembara Website/Backend/Laravel/Pengembara/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,7 +27,10 @@ Route::get('/checkout/success', 'CheckoutController@success')
 
 Route::prefix('admin')
     ->namespace('Admin')
+    ->middleware(['auth','admin'])
     ->group(function () {
         Route::get('/', 'DashboardController@index')
             ->name('dashboard');
     });
+
+Auth::routes(['verify' => true]);
